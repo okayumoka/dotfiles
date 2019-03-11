a4e3558fbd7d61e49b43c67373fe338833a5fe6d
okayu <okamoto@sikmi.com>
2019-03-11 10:38:13 +0900

    update

--- install.sh
+++ install.sh
@@ -42,6 +42,15 @@ elif [ $env = "ubuntu" ]; then
 fi
 
 #-------------------------------------
+# alacritty
+#-------------------------------------
+
+if [ $env = "mac" ]; then
+  ln -sf ${current}/alacritty/alacritty.yml.mac ~/.config/alacritty/alacritty.yml
+fi
+
+
+#-------------------------------------
 # Git
 #-------------------------------------
 ln -sf ${current}/git/_gitconfig ~/.gitconfig
